15cef0a97f1c3aa2b9b8b9b8fa5ee4883a61380d
Pablo Coviza <vanityone@yahoo.es>
2022-05-30 20:54:03 +0200

    subo cambios con respecto al detalle de los pedidos

--- src/Controllers/DetallesController.php
+++ src/Controllers/DetallesController.php
@@ -5,24 +5,28 @@ namespace App\Controllers;
 use App\Core\AbstractController;
 use App\Core\EntityManager;
 use App\Entity\Detalle;
+use App\Entity\Pedido;
 use Doctrine\ORM\Mapping\Entity;
 use App\Repository\DetalleRepository;
+use App\Repository\PedidoRepository;
 use Doctrine\Common\Util\Debug;
 
 class DetallesController extends AbstractController
 {
-    public function showDetalles()
+    public function showDetalles($pedidoNum)
     {
         
         $entityManager = (new EntityManager())->get();
         $detalleRepository = $entityManager->getRepository(Detalle::class);
-        $detalles = $detalleRepository->findAll();
+        // $detalles = $detalleRepository->findBy(['pedidoNum' => '601']);
+        $pedidoRepository = $entityManager->getRepository(Pedido::class);
+        $pedido = $detalleRepository->findBy(['pedido' => $pedidoNum]);
         /* echo "<pre>";
         Debug::dump($detalles);
         die(); */
 
         $this->render("detalles.html.twig", [
-            "resultados" => $detalles
+            "resultados" => $pedido
         ]);
     }
 }
